3026136567f76206795f3f75e0855c3fdc101401
Vincent on Lenovo <vlassiter@outlook.com>
2021-03-03 13:14:31 -0500

    added about page

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+
+
+    return (
+            <>
+             <h1>Hi welcome to my Awesome site!</h1>
+            </>
+    )
+
+}
+
+export default About
+
+
