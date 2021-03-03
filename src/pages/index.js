641dd257fb0a7219b4ee0b794ffe0969f2dcdce5
Vincent on Lenovo <vlassiter@outlook.com>
2021-03-03 14:35:18 -0500

    added picture

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import { StaticImage } from "gatsby-plugin-image"
 
 // markup
 const IndexPage = () => {
@@ -13,6 +14,8 @@ const IndexPage = () => {
 
       <title>Home Page</title>
       <Layout pageTitle="Vincent's Amazing Home Page"></Layout>
+
+      <StaticImage alt="Vincent's Picture" src="https://quiin.s3.us-east-1.amazonaws.com/users/pictures/004/966/881/medium/profile_-_Copy.jpg?1614791757"></StaticImage>
   
       <p>My great site home page</p>
 
